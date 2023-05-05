e0b911220b9eee54d9cd1e2fd35aaacdf0bc326e
lejler <layla.shokri@gmx.de>
2023-05-05 15:45:39 +0200

    added aufgabe 3

--- src/main/java/Auftrag3/WriteBookstoreData3.java
+++ src/main/java/Auftrag3/WriteBookstoreData3.java
@@ -11,25 +11,22 @@ package Auftrag3;
  *               
  */
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WriteBookstoreData3 {
+
+	public static final String filePath = "src/main/resources/buchhandlung3.xml";
 
 	public static void main(String[] args) {
 
@@ -39,8 +36,48 @@ public class WriteBookstoreData3 {
 		buchliste.add(new Buch("XQuery Kick Start", "James McGovern", 49.99));
 		buchliste.add(new Buch("Learning XML", "Erik T. Ray", 39.95));
 
-		//Add your code here
-		
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.newDocument();
+
+			Element root = doc.createElement("buchhandlung");
+			doc.appendChild(root);
+
+			populateXml(doc, root, buchliste);
+
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File(filePath));
+			transformer.transform(source, result);
+		} catch (ParserConfigurationException | TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public static void populateXml(Document doc, Element root, List<Buch> books) {
+		for (Buch buch : books) {
+			Element book = doc.createElement("book");
+			root.appendChild(book);
+
+			Element title = doc.createElement("titel");
+			title.appendChild(doc.createTextNode(buch.getTitel()));
+			book.appendChild(title);
+
+			Element author = doc.createElement("autor");
+			author.appendChild(doc.createTextNode(buch.getAutor()));
+			book.appendChild(author);
+
+			Element price = doc.createElement("preis");
+			price.appendChild(doc.createTextNode(String.valueOf(buch.getPreis())));
+			book.appendChild(price);
+		}
+
 	}
 
 }
